e54eac6c569b5cd112aa7a73e9be2b4b75181eca
Selim <selimgomri@gmail.com>
2022-02-08 12:10:15 +0100

    api working on console

--- mini-pokedex/src/app/list-pokemon/list-pokemon.component.ts
+++ mini-pokedex/src/app/list-pokemon/list-pokemon.component.ts
@@ -1,15 +1,17 @@
+import { ApiService } from './../service/api.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-list-pokemon',
+  selector: 'list-pokemon',
   templateUrl: './list-pokemon.component.html',
-  styleUrls: ['./list-pokemon.component.scss']
+  styleUrls: ['./list-pokemon.component.scss'],
 })
 export class ListPokemonComponent implements OnInit {
+  constructor(private apiService: ApiService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  fetchKantoPokemon() {
+    this.apiService.fetchKantoPokemon();
   }
-
 }
--- mini-pokedex/src/app/service/api.service.ts
+++ mini-pokedex/src/app/service/api.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  constructor() {}
 
-  constructor() { }
+  fetchKantoPokemon() {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+      .then((response) => response.json())
+      .then(function (allpokemon) {
+        allpokemon.results.forEach(function (pokemon) {
+          fetchPokemonData(pokemon);
+        });
+      });
+  }
+
+  fetchPokemonData(pokemon) {
+    let url = pokemon.url; // <--- this is saving the pokemon url to a      variable to us in a fetch.(Ex: https://pokeapi.co/api/v2/pokemon/1/)
+    fetch(url)
+      .then((response) => response.json())
+      .then(function (pokeData) {
+        console.log(pokeData);
+      });
+  }
 }
